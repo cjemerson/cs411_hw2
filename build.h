32621b83e8c74d7bf6684ef3594188db8667ae2b
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-09-21 15:32:13 -0800

    Add Test File and Dummy Header

---
+++ build.h
@@ -0,0 +1,23 @@
+// build.h
+// Charles Emerson
+// 21 September 2019
+// Updated: 21 September 2019
+//
+// For CS 411 Fall 2019
+// Source for function build
+// Solution for Assignment 2, Exercise A
+
+
+#ifndef FILE_BUILD_H_INCLUDED
+#define FILE_BUILD_H_INCLUDED
+
+#include <vector>
+
+int build(int w, int e, std::vector<std::vector<int>> bridges)
+{
+    (void) w, (void) e, (void) bridges;
+
+    return 0;
+}
+
+#endif // #ifndef FILE_BUILD_H_INCLUDED
