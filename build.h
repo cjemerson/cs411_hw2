a7520c5ba8e604368721d8c04a8e70487f7f6c5d
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-09-21 16:12:46 -0800

    Basic solution

--- build.h
+++ build.h
@@ -12,12 +12,57 @@
 #define FILE_BUILD_H_INCLUDED
 
 #include <vector>
+#include<algorithm>
+#include "build_utility.h"
+
+void sort(std::vector<std::vector<int>> & bridges)
+{
+    std::sort(bridges.begin(), bridges.end(), [](const auto & a, const auto & b)
+    {
+        return a[0] < b[0];
+    });
+}
+
+int build_worker(const std::vector<std::vector<int>> & bridges, int i = 0, int w = 0, int e = 0)
+{
+    const int BRIDGES_SIZE = int(bridges.size());
+
+    if (i >= BRIDGES_SIZE)
+    {
+        return 0;
+    }
+
+    auto max = 0;
+    auto current_w = bridges[i][0];
+    auto current_e = bridges[i][1];
+
+    // If we can place a bridge
+    if (current_w >= w && current_e >= e)
+    {
+        // Try with the bridge
+        auto temp = bridges[i][2] + build_worker(bridges, i + 1, current_w + 1, current_e + 1);
+        max = (max < temp)? temp : max;
+    }
+
+    // Try without the bridge
+    auto temp = build_worker(bridges, i + 1, w, e);
+    max = (max < temp)? temp : max;
+
+    return max;
+}
 
 int build(int w, int e, std::vector<std::vector<int>> bridges)
 {
     (void) w, (void) e, (void) bridges;
 
-    return 0;
+    if (bridges.empty())
+    {
+        return 0;
+    }
+
+    sort(bridges);
+
+    return build_worker(bridges);
 }
 
 #endif // #ifndef FILE_BUILD_H_INCLUDED
